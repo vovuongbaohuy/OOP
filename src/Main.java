4aea37658d4a520930d82352eb0853d6ce1202da
JTrinh03 <107232361+JTrinh03@users.noreply.github.com>
2023-12-22 02:41:38 +0700

    Hao push len

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
             System.out.println("value of i: " + i);
+            System.out.println("Hello");
         }
     }
 }
