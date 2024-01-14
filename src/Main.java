f69559a32e4f0e557bee7d3e76b3b2665fa2c917
Huy Vo <10421021@student.vgu.edu.vn>
2024-01-14 11:14:37 +0100

    delete redundant variables in Main class

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,3 @@
-import javax.swing.*;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         Frame1 frame1 = new Frame1();
